97832ce87af681a5bdae7a1c4f5ebd478d626548
Pirrabu <pirraburam@gmail.com>
2023-12-31 13:52:03 +0530

    Initial Commit

---
+++ mydb.py
@@ -0,0 +1,11 @@
+import mysql.connector
+
+database = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    passwd='root'
+)
+
+cursorObject = database.cursor()
+
+cursorObject.execute('CREATE DATABASE dcrm')
